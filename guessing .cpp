b149802183a191989624ed6c6302f69536a926e9
pooja-8252 <155881896+pooja-8252@users.noreply.github.com>
2024-01-13 22:40:06 +0530

    Add files via upload

---
+++ guessing .cpp
@@ -0,0 +1,29 @@
+//NUMBER GUESSING GAME MADE BY KALYANI SINGH....
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;                
+int main() {
+
+    cout<<"*********Made by ..."<<endl;
+    srand(time(0));                               
+    int RandomNum = rand() % 100 + 1;              
+    int guess;
+    int attempt = 0; // for counting no of Attempts.
+    cout << "***Welcome to the Number Guessing Game!!****\n\n";
+    cout<< "Try to guess number between 1 and 100.\n\n";
+    do {
+        cout << "Enter your Number by Guessing:";            
+        cin >> guess;
+        if (guess < RandomNum) {
+            cout << "Try Again!!.. Your Guessed Number is too low.\n\n";
+        } else if (guess > RandomNum) {
+            cout << "Try Again!!.. Your Guessed Number is too high.\n\n";
+        } else {
+            cout << "Congratulations!!.. You guessed the Correct Number in " << attempt << " attempts.\n";
+            break;   
+        }
+        attempt++;     
+    } while (true);   
+    return 0;
+}
